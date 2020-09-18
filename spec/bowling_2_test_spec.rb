35aaea48725f6576e65de85ad8fbd938b2e8a409
Chloe <chloe.wong@madetech.com>
2020-09-18 10:20:13 +0100

    second test pass

--- spec/bowling_2_test_spec.rb
+++ spec/bowling_2_test_spec.rb
@@ -4,4 +4,8 @@ describe "returns score for 1 bowl" do
     it "returns a score of 1 when a 1 is bowled" do
         expect(Bowling.new.bowling_game_score(1)).to eq(1)
     end
+
+    it "returns a score of 6 when a 6 is bowled" do
+        expect(Bowling.new.bowling_game_score(6)).to eq(6)
+    end
 end
